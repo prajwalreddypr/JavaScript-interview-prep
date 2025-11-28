9bbe0db8140cdeb27f29a9892d69496334714a86
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-28 21:56:09 +0100

    updates

--- Day 1/callbacks.js
+++ Day 1/callbacks.js
@@ -1,7 +1,23 @@
-function fetchData() {
-    setTimeout(() => {
-        console.log("Hello after 5 seconds");
-    }, 5000);
-}
+// function fetchData() {
+//     setTimeout(() => {
+//         console.log("Hello after 5 seconds");
+//     }, 5000);
+// }
 
-fetchData();
+// fetchData();
+
+// function greet(name){
+//     console.log(`Heelo ${name}`);
+// }
+
+// setTimeout(greet, 4000, "Prajwal");
+
+
+
+let timer  = setTimeout(() => {
+    console.log("This will not run");
+}, 3000);
+
+
+clearTimeout(timer);
+console.log("Timeout Cleared");
