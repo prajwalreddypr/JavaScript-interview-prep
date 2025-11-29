b4f8f0dd3f9d04575dab5d8928ffbd85ba46baea
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-29 23:12:33 +0100

    updated files

---
+++ Day 1/arrays1.js
@@ -0,0 +1,31 @@
+// let arr = [1,2,3];
+// console.log(arr[0])
+// arr[0] = 2;
+// console.log(arr)
+// arr.push(4);
+// console.log(arr);
+
+// //last element remove
+// arr.pop(); 
+// console.log(arr);
+
+// //first element remove
+// arr.shift();
+// console.log(arr);
+
+// //adding to the first position
+// arr.unshift(99);
+// console.log(arr);
+
+// arr.unshift(23,34);
+// console.log(arr);
+
+// //removed last element.
+// arr.pop();
+// console.log(arr)
+
+let arr = [10,20,30];
+console.log(arr.indexOf(10));
+console.log(arr.includes(20));
+console.log(arr.includes(21));
+console.log(arr.join(","));
--- Day 1/tempCodeRunnerFile.js
+++ Day 1/tempCodeRunnerFile.js
@@ -1,12 +1,1 @@
-//functions being returned from other functions
-
-
-function outer(){
-    function inner(){
-        console.log("Hello Prajwal");
-    }
-    return inner;
-}
-
-let returnedFuncVar = outer();
-console.log(returnedFuncVar);
+console.log(arr);
