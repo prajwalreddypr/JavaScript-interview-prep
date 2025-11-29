3f34ba774bbb48c953368efd1ba5a59648abf585
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-29 23:41:34 +0100

    updated files

--- Day 1/arrays1.js
+++ Day 1/arrays1.js
@@ -24,8 +24,49 @@
 // arr.pop();
 // console.log(arr)
 
-let arr = [10,20,30];
-console.log(arr.indexOf(10));
-console.log(arr.includes(20));
-console.log(arr.includes(21));
-console.log(arr.join(","));
+// let arr = [10,20,30];
+// console.log(arr.indexOf(10));
+// console.log(arr.includes(20));
+// console.log(arr.includes(21));
+// console.log(arr.join(","));
+
+
+// //MAP
+// let arr = [1,2,3,4,5,6,7,8];
+// let doubled = arr.map(x => x * 2);
+// console.log(doubled);
+
+// //FILTER
+// let arr1 = [1,2,3,4,5,6];
+// let even = arr.filter(x => x % 2 == 0);
+// console.log(even);
+
+// //REDUCE
+// let arr2 = [1,2,3,4,5];
+// let sum = arr2.reduce((total, number) => total + number, 0);
+// console.log(sum);
+
+// //REDUCE
+// let arr3 = [3,4,6,7,8];
+// let product = arr3.reduce((total, num) => total * num, 1);
+// console.log(product);
+
+
+// //SORT
+// let arr = [3,2,4,7,3,8,0];
+// arr.sort((a, b) => a - b);
+// let desc = arr.sort((a, b) => b-a);
+// console.log(arr);
+// console.log(desc);
+
+// //FIND
+// let arr1 = [1,3,5,7,9,43];
+// let firstBig = arr1.find(x => x >5);
+// console.log(firstBig);
+
+// //FIND1
+// let arr2 = [45,324,657,34,546];
+// let firstSmall = arr2.find(x => x < 100);
+// console.log(firstSmall);
+
+
