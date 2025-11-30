3f76befa4c6d186bf7610b625b375b5c0034d7f7
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-30 03:17:32 +0100

    updated files

---
+++ Day 1/conditionals.js
@@ -0,0 +1,23 @@
+// x=10;
+
+// if(x > 10){
+//     console.log("fdfd");
+// } else if(x == 10){
+//     console.log("Hatr Noun");
+// } else{
+//     console.log("I am dead");
+// }
+
+//if number is positive negative or zero
+
+// let number = 0;
+
+// if (number > 0){
+//     console.log("Positive");
+// }else if(number == 0){
+//     console.log("ZERO");
+// }else if(number < 0){
+//     console.log("Negative");
+// }
+
+
