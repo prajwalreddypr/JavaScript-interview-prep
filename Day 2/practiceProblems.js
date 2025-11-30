d18f521dba0d7b94e3b88d3e63900a408ee34aef
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-30 11:34:47 +0100

    updated files

---
+++ Day 2/practiceProblems.js
@@ -0,0 +1,58 @@
+//add 2 numbers
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(5,6));
+
+// Convert a number to a string.
+// let num = 23;
+// let string = String(num);
+// console.log(string)
+// console.log(typeof(string))
+
+
+
+
+// Get the last 3 characters of a string.
+// let str = "heyThereIamInParis";
+// console.log(str.slice(-3));
+
+
+// Count how many vowels are in a string.
+// let str = "helloWorldBonjour";
+// let vowels = "aeiouAEIOU";
+// let count = 0;
+
+// for(let char of str){
+//     if(vowels.includes(char)){
+//         count += 1;
+//     }
+// }
+// console.log(count);
+
+// Check if a string contains "@gmail.com".
+// let email = "reddyprajwal2000@gmail.com";
+// console.log(email.includes("@gmail.com"));
+
+
+// Capitalize the first letter of a string.
+// let greeting = "hello Paris";
+// console.log(greeting[0].toUpperCase() + greeting.slice(1));
+
+// // Replace all spaces in a string with "-".
+
+// let str = "Hello Vitry Sur Seine, HOW are you doing ?"
+// //replace() -> only replaces the 1st occ. replaceAll() -> replaces all the occ.
+// let newStr = str.replaceAll(" ", "-");
+// console.log(newStr);
+
+
+// Given "12345", sum all its digits.
+let 
+
+
+// Check if two strings are equal ignoring case.
+
+// Reverse a number (e.g., 123 → 321).
