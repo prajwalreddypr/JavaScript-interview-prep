c83baea24a0abef0d51b0256659d4251e0cc1dd2
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-30 03:12:21 +0100

    updated files

---
+++ Day 1/functions1.js
@@ -0,0 +1,51 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(3,5));
+
+// function greet(name = "guest"){
+//     return `Hello ${name}`;
+// }
+
+// console.log(greet("Prajwal"));
+// console.log(greet());
+
+//fucntion that returns square of a number
+
+// function squared(num){
+//     return num ** 2;
+// }
+
+// console.log(squared(2));
+// console.log(squared(4));
+
+
+
+//function that returns the last element of the array
+
+// let nums = [1,2,3,4,5];
+
+// function lastElem(arr){
+//     return arr[arr.length - 1];
+// }
+
+// console.log(lastElem(nums));
+
+
+
+
+///Write function that counts vowels.
+
+// function countVowels(str){
+//     let count = 0
+//     let vowels = "aeiouAEIOU";
+//     for (let char of str){
+//         if (vowels.includes(char)){
+//             count += 1
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countVowels("Hello Guys"))
