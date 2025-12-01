b0dc4eb107ace7265926ccd925e93c9fabc8e849
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-12-01 19:36:21 +0100

    updates day 3

---
+++ Day 3/stringsProblems.js
@@ -0,0 +1,64 @@
+//PROBLEMS ON STRING
+
+//REVERSE A STRING
+// const str = "bonjourParis";
+// let rev = ""
+
+// for(let i=str.length-1; i >=0; i--){
+//     rev += str[i];
+// }
+
+// console.log(rev);
+
+
+
+
+
+//COUNTING VOWELS IN A STRING
+// const str = "ILoveJavaScript";
+// const vowels = "aeiouAEIOU";
+// let count = 0;
+
+// for(let char of str.toLowerCase()){
+//     if(vowels.includes(char)){
+//         count += 1;
+//     }
+// }
+
+// console.log(count);
+
+
+
+//CHECKING IF A GIVEN STRING IS PALINDROME
+//method = reverse the string nd compare.
+
+// let str = "bonjourEmily";
+// let rev = "";
+
+// for(let i=str.length-1; i >=0; i--){
+//     rev += str[i];
+// }
+
+// console.log(str == rev);
+//OR
+
+// const str = "malayalam";
+// const rev = str.split("").reverse().join("");
+
+// console.log(str === rev);
+
+
+
+//COUNTING OCC OF A CHARACTER
+// const str = "EmilyInParis";
+// const charToCount = "i";
+// let count = 0;
+
+// for(let char of str.toLowerCase()){
+//     if(charToCount == char){
+//         count += 1;
+//     }
+// }
+
+// console.log(count);
+
