4ac328815956d5cfd73ad3041fda0b70511b955b
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-12-04 10:56:22 +0100

    final day

--- Day 5/warmup.js
+++ Day 5/warmup.js
@@ -134,6 +134,19 @@
 
 
 //MAX ELEMENT IN A ARRAY
-let maxElem = function(arr){
-    
-}
+// let maxElem = function(arr){
+//     let maxNum = arr[0];
+
+//     for(let i=1; i < arr.length; i++){
+//         if(arr[i] > maxNum){
+//             maxNum = arr[i];
+//         }
+//     }
+//     return maxNum;
+
+// }
+
+// console.log(maxElem([7,1,2,3,4,5,6]));
+
+
+//
