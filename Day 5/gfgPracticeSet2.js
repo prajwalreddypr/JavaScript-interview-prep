86df8894ce26b157e1fb6c427ffdddfa45c5a588
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-12-04 01:37:25 +0100

    updates day 6

---
+++ Day 5/gfgPracticeSet2.js
@@ -0,0 +1,19 @@
+//LONGEST WORD IN A STRING
+//str = "Paris is a wonderful city";
+// let longestWord = function(str){
+//     let words = str.split(" ");
+//     let longest = " ";
+
+//     for(let word of words){
+//         if(word.length > longest.length){
+//             longest = word
+//         }
+//     }
+//     return longest;
+// }
+
+// console.log(longestWord("Paris is a woderful city"));
+
+
+
+//
